1998cfaf8cf538085ad989c8404d0db9cfd50b37
Michal Plucinski <mic.plu@wp.pl>
2023-03-11 01:31:00 +0100

    making some order in functions

--- algos.py
+++ algos.py
@@ -1,6 +1,12 @@
 import random
 import time
 
+#filling tab
+def tabFill(arr, quantitymax=1000, valuemax=100):
+    for i in range(0, quantitymax): #packing tab
+        arr.append(random.randrange(1, valuemax)) #from 1 to valuemax
+
+
 #Insertion Sort
 def iS(arr):
     # Traverse through 1 to len(arr)
@@ -17,20 +23,20 @@ def iS(arr):
         arr[j + 1] = key
 
 # Selection Sort
-def sS():
+def sS(arr):
 # Traverse through all array elements
-    for i in range(len(A)):
+    for i in range(len(arr)):
 
         # Find the minimum element in remaining
         # unsorted array
         min_idx = i
-        for j in range(i + 1, len(A)):
-            if A[min_idx] > A[j]:
+        for j in range(i + 1, len(arr)):
+            if arr[min_idx] > arr[j]:
                 min_idx = j
 
         # Swap the found minimum element with
         # the first element
-        A[i], A[min_idx] = A[min_idx], A[i]
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
 # Heap Sort
 def heapify(arr, N, i):
@@ -77,14 +83,26 @@ def main():
 
     r = []
 
-    for i in range(0, 1000): #packing tab
-        r.append(random.randrange(1, 150)) #from 1, 150
+    tabFill(r, 1000, 150)
+
     start_time = time.time()
+    sS(r.copy())
     stop_time = time.time() - start_time
 
 
 
     print("--- %s seconds ---" % (time.time() - start_time))
+
+
+
+    start_time = time.time()
+    iS(r.copy())
+    stop_time = time.time() - start_time
+
+
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+
 
 
 
